5c6736d8a6c8c3455953d51e8508055ea2b4d9d9
matheince <57901624+matheince@users.noreply.github.com>
2019-11-25 14:37:00 +0900

    사용자 ID생성 Firebase를 통한 로그인 버튼만으로 사용자 ID:MahienceInst@gmail.com 완성

--- app/src/main/java/com/e/howlssns/LoginActivity.kt
+++ app/src/main/java/com/e/howlssns/LoginActivity.kt
@@ -2,11 +2,29 @@ package com.e.howlssns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    var auth: FirebaseAuth?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            createAndLoginEmail()
+        }
+
+    }
+    fun createAndLoginEmail(){
+        auth?.createUserWithEmailAndPassword("mathienceinst@gmail.com","123456")?.addOnCompleteListener {
+            task ->
+            if(task.isSuccessful){
+                Toast.makeText(this,"아이디 생성 성공",Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
