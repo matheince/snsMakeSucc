aa880d4e5aa3e7a2104e0b15c2badd72c12da2ec
matheince <57901624+matheince@users.noreply.github.com>
2019-11-25 15:56:22 +0900

    하울강좌 "Firebase로 안드로이드 SNS 앱 만들기" - 2일차

--- Readme.md
+++ Readme.md
@@ -1,15 +1,12 @@
-2019년 11월 24일 작업내용
-dependencies 세팅에 다음 줄을 추가해야
- implementation 'com.android.support:appcompat-v7:27.0.2'
- implementation 'com.android.support:design:27.0.2'
+2019년 11월 25일 작업내용
+ android:inputType="textPassword" 입력값이 ***** 처리됨
+ 파이버베이스에 사용설정및 LoginActivity 에 다음 함수및 activity_login.xml 에 아이디 지정만으로 생성됨
+ fun createAndLoginEmail(){
+         auth?.createUserWithEmailAndPassword(email_EditText.text.toString(),password_EditText.text.toString())?.addOnCompleteListener {
+             task ->
+             if(task.isSuccessful){
+                 Toast.makeText(this,"아이디 생성 성공",Toast.LENGTH_LONG).show()
+             }
+         }
 
-이 TextInputLayout을 사용할 수 있음
-<com.google.android.material.textfield.TextInputLayout>
-
-다음의 오류는 폰에서 앱을 지웠을 때 설정>일반>앱 및 알림> 중지된 앱을 강제 종료후 해결
-$ adb shell am start -n "com.e.howlssns/com.e.howlssns.LoginActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
-
-앱이름은 처음 실행될 Activity에서 Application 안에 설정
- android:label="mathienceInst"
-
-Android Keyboard 자동시 화면 밀림 현상은 AndroidManifest.xml 로 이동 <Actitity 안에 android:windowSoftInputMode = "adjustNothing" 설정으로 해결 
+     }
--- app/src/main/java/com/e/howlssns/LoginActivity.kt
+++ app/src/main/java/com/e/howlssns/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.e.howlssns
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -19,12 +21,40 @@ class LoginActivity : AppCompatActivity() {
 
     }
     fun createAndLoginEmail(){
-        auth?.createUserWithEmailAndPassword("mathienceinst@gmail.com","123456")?.addOnCompleteListener {
+        auth?.createUserWithEmailAndPassword(email_EditText.text.toString(),password_EditText.text.toString())?.addOnCompleteListener {
             task ->
             if(task.isSuccessful){
                 Toast.makeText(this,"아이디 생성 성공",Toast.LENGTH_LONG).show()
             }
+            else if(task.exception?.message.isNullOrEmpty()){
+                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+            } else{
+                signinLogin()
+            }
         }
 
     }
+    fun signinLogin() {
+        auth?.signInWithEmailAndPassword(
+            email_EditText.text.toString(),
+            password_EditText.text.toString()
+        )?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                //Toast.makeText(this,"로그인이 성공했습니다.",Toast.LENGTH_LONG).show()
+                moveMainPage(auth?.currentUser)
+            } else {
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+    }
+    fun moveMainPage(user : FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+    }
+
 }
